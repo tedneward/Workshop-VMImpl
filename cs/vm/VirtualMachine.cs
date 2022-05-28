dc5335dfa3f81d1dfc2dfae54c41dd2bcc524031
Ted Neward <ted@tedneward.com>
2022-05-28 04:47:16 -0700

    Forgot to add Trace method

--- cs/vm/VirtualMachine.cs
+++ cs/vm/VirtualMachine.cs
@@ -11,6 +11,11 @@ public class VirtualMachine
 {
     int IP = 0;
     bool trace = false;
+    private void Trace(string message)
+    {
+        if (trace)
+            Console.WriteLine("TRACE: {0}", message);
+    }
 
     public void Execute(Bytecode[] code)
     {
@@ -20,13 +25,16 @@ public class VirtualMachine
             {
                 case Bytecode.NOP:
                     // Do nothing!
+                    Trace("NOP");
                     break;
                 case Bytecode.DUMP:
+                    Trace("DUMP");
                     Console.WriteLine("IP: {0}", IP);
                     Console.WriteLine("code: [{0}]", String.Join(",", code));
                     break;
                 case Bytecode.TRACE:
                     trace = !trace;
+                    Trace("TRACE");
                     break;
                 default:
                     throw new Exception("Unrecognized opcode: " + code[IP]);
