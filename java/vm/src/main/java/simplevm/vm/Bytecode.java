ec0bc8d6b9ccd06bc870d8923b05cc963b18be4a
Ted Neward <ted@tedneward.com>
2024-04-07 22:05:47 -0700

    Update Bytecode.java to remove HALT

--- java/vm/src/main/java/simplevm/vm/Bytecode.java
+++ java/vm/src/main/java/simplevm/vm/Bytecode.java
@@ -9,8 +9,7 @@ public class Bytecode {
     public static final int DUMP = 1;
     public static final int TRACE = 2;
     public static final int PRINT = 3;
-    public static final int HALT = 4;
-    public static final int FATAL = 5;
+    public static final int FATAL = 4;
 
     // Stack manipulation
     public static final int CONST = 10;
